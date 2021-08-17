3e25f8e257433de95a4a8d8130c9d7ffd5005dae
sayaka-tamura <arccycle@gmail.com>
2021-08-18 00:07:26 +0900

    added composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd4b415174f09ae916dff0d047ab4560b533a393e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd4b415174f09ae916dff0d047ab4560b533a393e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
