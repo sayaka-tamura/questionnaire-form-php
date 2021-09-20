9928e67bd683885fe48a33d497121ecac2433885
sayaka-tamura <arccycle@gmail.com>
2021-09-20 22:46:27 +0900

    added bootstrap file

--- template/head.php
+++ template/head.php
@@ -1,4 +1,15 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+  <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <!--===============================================================================================-->
+
   <title>Questionary</title>
 </head>
