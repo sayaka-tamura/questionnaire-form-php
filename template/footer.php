68221729191e88156beeb9ed08b4e2388043a3d3
sayaka-tamura <arccycle@gmail.com>
2021-09-23 20:10:23 +0900

    get back to previous

--- template/footer.php
+++ template/footer.php
@@ -1,10 +1,10 @@
 <!--===============================================================================================-->
-  <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendors/bootstrap/js/popper.js"></script>
-  <script src="vendors/bootstrap/js/bootstrap.min.js"></script>
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendors/select2/select2.min.js"></script>
+  <script src="vendor/select2/select2.min.js"></script>
   <script>
     $(".selection-2").select2({
       minimumResultsForSearch: 20,
