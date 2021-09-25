b83a6f1db2d081ea7fdd548e99b9a10783e0eff0
sayaka-tamura <arccycle@gmail.com>
2021-09-25 10:45:03 +0900

    dm button test

--- web/index.php
+++ web/index.php
@@ -148,7 +148,7 @@
               <div class="my-4">New Publication Information</div>
               <div class="form-check ml-4">
                 <input type="hidden" name="dm" value="off">
-                <input class="form-check-input" id="dm-check" type="checkbox" name="dm" value="on" <?php if(isset($_SESSION['dm']) && $_SESSION['dm'] == "on"){echo 'checked="checked"';}?>>
+                <input class="form-check-input" id="dm-check" type="checkbox" name="dm" value="on" <?php if(isset($_SESSION['dm']) && $_SESSION['dm'] == "Request to send"){echo 'checked="checked"';}?>>
                 <label class="form-check-label" for="dm-check">Please send me the information</label>
               </div>
             </div>
