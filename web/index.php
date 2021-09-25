fce2c42452208237a61ac29850b92bc632db1b6a
sayaka-tamura <arccycle@gmail.com>
2021-09-25 10:03:02 +0900

    form checkbox test

--- web/index.php
+++ web/index.php
@@ -152,6 +152,19 @@
               </div>
             </div>
 
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label" for="flexCheckDefault">
+                Default checkbox
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+              <label class="form-check-label" for="flexCheckChecked">
+                Checked checkbox
+              </label>
+            </div>
+
             <div class="container-contact3-form-btn">
               <input type="submit" value="confirm" name="sub1"  class="contact3-form-btn">
             </div>
