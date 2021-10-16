d2068cfd8e650b76da44059bf2bb08efe150a5a0
sayaka-tamura <arccycle@gmail.com>
2021-10-16 11:01:43 +0900

    under investigation of DB error 4

--- web/submit.php
+++ web/submit.php
@@ -10,10 +10,10 @@
   require("template/dbconnect.php");
 
   //DB接続関数を dbconnet.php から呼び出して接続
-  $db = dbConnect();
+  $conn = dbConnect();
 
-  echo "$db";
-  
+  var_dump($conn);
+    
 /*
   // 接続設定
   $dbtype = "mysql";
