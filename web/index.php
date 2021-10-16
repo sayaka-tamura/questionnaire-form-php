35cbfc06b00f0838c5e3e6c87e74f040afb5a7ad
sayaka-tamura <arccycle@gmail.com>
2021-10-16 11:06:20 +0900

    check DB info

--- web/index.php
+++ web/index.php
@@ -6,6 +6,27 @@
     session_start();
     
     require("template/head.php"); 
+
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $db['dbname'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+
+    try {
+        $db = new PDO($dsn, $db['user'], $db['pass'], $options);
+        return $db;
+    } catch (PDOException $e) {
+        echo 'Error: ' . h($e->getMessage());
+    }
+
+    var_dump($_SERVER['CLEARDB_DATABASE_URL']);
+    var_dump($dsn);
+    var_dump($db['user']);
+    var_dump($db['pass']);
   ?>
 
   <body>
