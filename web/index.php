682ff3571bdca2c4ca9349e30d5533e1e28fc6cc
sayaka-tamura <arccycle@gmail.com>
2021-10-16 11:17:42 +0900

    check DB info 3

--- web/index.php
+++ web/index.php
@@ -7,9 +7,10 @@
     
     require("template/head.php"); 
 
-  /*
+  
     $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
     $db['dbname'] = ltrim($db['path'], '/');
+    /*
     $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
     $options = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
