d1e5a0342f4a3c5c6431cf18610ce360ec8e81eb
sayaka-tamura <arccycle@gmail.com>
2021-11-07 21:28:58 +0900

    removed charset from head

--- web/template/head.php
+++ web/template/head.php
@@ -1,8 +1,9 @@
 <head>
   <title>Questionary</title>
 
-  <!-- 文字コード指定 --!>
+  <!-- 文字コード指定
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  --!>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
   <!--===============================================================================================-->
