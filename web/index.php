71fe95342021ebc418f1dcfffe3951d3cf82820f
sayaka-tamura <arccycle@gmail.com>
2021-11-07 23:21:52 +0900

    test

--- web/index.php
+++ web/index.php
@@ -18,6 +18,9 @@
     // セッション変数設定
     $_SESSION['csrf_token'] = $csrf_token;
 
+    echo "$_SESSION'csrf_token': ".$_SESSION["csrf_token"]."<br />";
+    print('session_id()は '.session_id().' です。<br>');
+
     require("template/head.php"); 
   ?>
 
